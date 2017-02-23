9fabe9932c54a3117f854387e257caabb60c3281
Joshua Lawson <jacklegjosh@gmail.com>
2017-02-22 23:59:25 -0500

    Items updated

--- src/main/java/com/theironyard/models/Item.java
+++ src/main/java/com/theironyard/models/Item.java
@@ -151,91 +151,106 @@ public class Item {
             "%s is exceedingly strange");
 
     public static Item i8 = new Item("PHONOGRAPH",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "");
+            "You can't just TAKE one of these. They weigh, like, 85 pounds.",
+            "% is trying to take the phonograph. It would be funny if it wasn't so sad...",
+            "Where's the remote for this thing?",
+            "% is attempting to use the phonograph. It seems apparent that they don't know what they're doing.",
+            "It’s one of those old-timey record players, with the big horn on it. Dude… I bet Dark Side of the Moon would sound trippy on this!",
+            "What is % doing over by the phonograph? They better not be adjusting the speed...");
 
     public static Item i9 = new Item("TEA KETTLE",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "");
+            "You don't want to spend the rest of the time here lugging an iron kettle around.",
+            "If % wants to lug around an iron kettle for the rest of their time here, they can be your guest.",
+            "You probably wouldn't even get the water boiling in time!",
+            "You really wish % was helping you look for a way out of here instead of trying to make tea right now.",
+            "It’s smaller than a normal kettle and cast-iron. It has an oriental look to it. Somebody either takes tea very seriously or is kind of a hipster. It’s a fine line these days.",
+            "% is staring intently at the tea kettle.");
 
     public static Item i10 = new Item("PAINTING",
-            "",
-            "",
-            "",
-            "",
+            "It might be valuable, but you don't have pockets THAT big.",
+            "% seems to be contemplating the best way to make off with the painting.",
             "The PAINTING seems to be on hinges. You open it, revealing a safe. It’s a little cliche, but the possibility of finding swag is appealing!",
-            "It’s a Depression-era landscape, probably fine art, but to you, it's literally kinda depressing. There’s something odd about it.");
+            "% is getting up close and personal with that painting over there...",
+            "It’s a Depression-era landscape, probably fine art, but to you, it's literally kinda depressing. There’s something odd about it.",
+            " % seems transfixed by the painting.");
 
     public static Item i11 = new Item("GRANDFATHER CLOCK",
-            "",
-            "",
-            "",
-            "",
+            "It looks pretty good where it is.",
+            "You hope % isn't planning on trying to move that clock...",
             "You open the cabinet. It chimes… nuts. You were really hoping for that bird.",
-            "You wonder if it cuckoos or chimes. Hopefully, it cuckoos. You like birds.");
+            "% is getting handsy with the grandfather clock.",
+            "You wonder if it cuckoos or chimes. Hopefully, it cuckoos. You like birds.",
+            "% must really be interested in the time.");
 
     public static Item i12 = new Item("GLASS BOWL",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "");
+            "I mean, I guess you could take one of the decorative balls out, if you must.",
+            "What is % even doing over there?",
+            "What would you use it for? It's utterly useless. You hate it.",
+            "You should really tell % to quit messing around with that stupid decorative bowl.",
+            "It’s filled with those stupid useless decorative balls that look like they’re made out of tiny tumbleweeds or something. You hate it, and by extension, whoever put it here.",
+            "% is poking around the decorative glass bowl. Bully for them.");
 
     public static Item i13 = new Item("TABLE",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "");
+            "Yeah, that's probably not going to happen. Why do you make me say it? Do you want me to be that person? Is that what you want? I hate what you've turned me into.",
+            "% isn't letting a little thing like 'impossible' stop them from trying to take the table with them.",
+            "I'm sorry, I didn't mean what I said before. You're a good person, I just get antsy sometimes. Oh, and you can't use a table. Come on.",
+            "You weren't sure you knew what 'using' a table looked like before you saw % try to do it.",
+            "It's a pretty nice table, you guess. It's long and kind of tall. You think it's called a buffet, which only reminds you of how hungry you are.",
+            "Is that a... buffet table? Well, anyway, % is looking at it.");
 
     public static Item i14 = new Item("DESK",
             "Bruh, you can't take the desk!",
             "Looks like %s is trying to put the desk in their pocket?",
-            "",
-            "",
-            "",
-            "");
+            "You can't use a desk. What would you even use it for?",
+            "Who knows what % is doing?",
+            "A large desk. It would be rather intimidating to face your boss on the other side of this desk. It is littered with the typical desk miscellany, including a LAMP, a LETTER OPENER, a DESK CALENDAR and a PEN CUP.",
+            "% is rummaging through the desk. Hopefully they're finding something useful.",
+            );
 
     public static Item i15 = new Item("DRAWER",
-            "",
-            "",
-            "",
-            "",
-            "If it’s one thing you don’t need, it’s more paperclips. Wait, there’s something interesting in here… A key",
-            "");
+            "I mean, let's be real; what are the chances you're going to need a desk drawer in the future? We're just going to leave that here.",
+            "You can't even begin to guess what % is doing.",
+            "There's not much use for desk drawers. Unless you just enjoy pushing and pulling things.",
+            "You can say with certainty that % is using a desk drawer. Hm. So that's what that look's like.",
+            "If it’s one thing you don’t need, it’s more paperclips. Wait, there’s something interesting in here… A key!",
+            "% is rummaging through the drawers. They better not have found cooler stuff than you!");
 
     public static Item i16 = new Item("BOOKSHELF",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "");
+            "Dude. Seriously? We're not taking a bookshelf.",
+            "It looks like % is about to pull a bookshelf down on themselves.",
+            "A bookshelf is useful for one thing; holding books. It's doing that fine without your intervention.",
+            "When you first saw %, you had them pegged as someone who really appreciated shelving. Judging by their interest in that bookshelf, you were right. Again.",
+            "It smells of rich mahogany and there are many leather-bound books. They’re probably valuable.",
+            "You wonder what %'s taste in books is...'");
 
     public static Item i17 = new Item("BOOK",
-            "",
-            "",
-            "",
-            "",
-            "It's called 'Talking to Your Dog About Communism' that sounds delightful.",
+            "Ooh, a book.",
+            "It's hard to say for certain, but it looks like % just stuffed a book in their pocket.",
+            "Books are for readin', not for usin'.",
+            "Who can say what % is doing over there with that book?",
+            "It's called 'Talking to Your Dog About Communism'. That sounds delightful.",
             "");
     public static Item i18 = new Item("KEY",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "");
+            "Yay, a key!",
+            "Aw, man! % has a key! What have you found? Just dumb stuff. You should feel bad about yourself.",
+            "It's not much use on its own...",
+            "Watching % use a key on nothing has left you feeling somewhat empty inside...",
+            "It has a shiny quality to it.",
+            "Aw. % found a key. You've only found stupid stuff.");
+    public static Item i19 = new Item("CHAIR",
+            "What is your obsession with trying to take furniture, huh?",
+            "% is never going to be able to take that chair, but that won't stop you from watching them try.",
+            "As much as you deserve a rest, you have things to do.",
+            "% is taking a load off.",
+            "It’s leather and grand. It looks quite old and comfy, but well taken care of. You could imagine listening to a jazz record while smoking a cigar in it. And you don’t even like any of those things!",
+            "% is giving that old chair a good looking-at.");
+    public static Item i20 = new Item("PORTRAIT",
+            "I give up; you want this painting, take it. See if I care.",
+            "It looks like % is giving taking that portrait the ol' college try. You envy their spirit.",
+            "You drew a mustache upon the man in the painting. That ought to take him down a notch or two!",
+            "% is drawing a mustache on that portrait over there. Take that, portrait!",
+            "there is a large painting of stately man with a far away look in his eye. He’s doing that Napoleon thing where his hand is tucked into his jacket. Is this guy for real?",
+            "% is having a staring contest with the portrait. The dude in the portrait is winning.");
 
 
 
@@ -243,7 +258,7 @@ public class Item {
         Room.items.add(i1); Room.items.add(i2); Room.items.add(i3); Room.items.add(i4); Room.items.add(i5);
         Room.items.add(i6); Room.items.add(i7); Room.items.add(i8); Room.items.add(i9); Room.items.add(i10);
         Room.items.add(i11); Room.items.add(i12); Room.items.add(i13); Room.items.add(i14); Room.items.add(i15);
-        Room.items.add(i16); Room.items.add(i17); Room.items.add(i18);
+        Room.items.add(i16); Room.items.add(i17); Room.items.add(i18); Room.items.add(i19); Room.items.add(i20);
     }
 
 
